fe3dcd66a6db1af6f887a200d360772db4b72430
“用cwf <308466155@qq.com>
2017-09-30 18:15:51 +0800

    职位管理

--- js/job_manage_release.js
+++ js/job_manage_release.js
@@ -12,3 +12,9 @@ $("textarea").on("input",function(){
 $("textarea").on("focus",function(){
     $(this).closest(".release_list").find(".formtip").remove();
 })
+
+var salarychoice= $.salarychoice({
+    data:["面议","1k","2k","3k","4k","5k","6k","7k","8k"],
+    input:"#salarychoice",
+    width:"218px"
+})
