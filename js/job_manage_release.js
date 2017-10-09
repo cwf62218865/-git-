c41dace0ede7dc8212aa6c7ea8ed529ca3e636b5
“用cwf <308466155@qq.com>
2017-10-09 15:19:46 +0800

    职位管理

--- js/job_manage_release.js
+++ js/job_manage_release.js
@@ -18,3 +18,43 @@ var salarychoice= $.salarychoice({
     input:"#salarychoice",
     width:"218px"
 })
+//模板名称切换
+$(".template_names").eq(0).on("click",function(){
+    $(".template_names").eq(0).addClass("template_name_choiced");
+    $(".template_names").eq(1).removeClass("template_name_choiced");
+    $(".job_template_msg").show();
+    $(".do_job_template_msg").hide();
+});
+$(".template_names").eq(1).on("click",function(){
+    $(".template_names").eq(1).addClass("template_name_choiced");
+    $(".template_names").eq(0).removeClass("template_name_choiced");
+    $(".job_template_msg").hide();
+    $(".do_job_template_msg").show();
+});
+
+//选择模板
+$(".job_template_msg .job_template_msgbtn").one("click",function(){
+    var _this=$(this);
+    _this.prev().css({"background-color":"#a8c9ea","color":"#fff","border-color":"#a8c9ea"});
+    _this.find("use").attr("xlink:href","#icon-zhengque2");
+    _this.append("<span style='float: right;margin-left: 10px;font-size: 14px;line-height: 36px'>已添加至岗位职责</span>")
+    var content1=_this.prev().html();
+    var content2=$("#job_duty_textarea").val();
+    var length=content2.split(".").length;
+
+    if(content2){
+        $("#job_duty_textarea").val(content2+"\r\n"+length+"."+content1);
+        $("#job_duty_input").val(content2+"\r\n"+length+"."+content1);
+    }else{
+        $("#job_duty_textarea").val(length+"."+content1);
+        $("#job_duty_input").val(length+"."+content1);
+    }
+});
+
+$("#job_duty_textarea").keyup(function (e) {
+    if (e.keyCode == 13) {
+        var content2=$(this).val();
+        var length=content2.split(".").length;
+        $("#job_duty_textarea").val(content2+length+".");
+    }
+});
